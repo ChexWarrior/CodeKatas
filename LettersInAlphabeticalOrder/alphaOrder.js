9a6f899bb7c8a1c7b44447e89acfcc68e64c2374
ChexWarrior <aplehm@gmail.com>
2015-08-19 19:18:07 -0400

    Add Letters In Alphabetical Order kata (and solution)

---
+++ LettersInAlphabeticalOrder/alphaOrder.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var processButton = document.querySelector("input[name='process']");
+    processButton.addEventListener('click', function() {
+        var processInput = document.querySelector("input[name='input']");
+        var inputValue = processInput.value;
+        console.log(inputValue);
+        alert(isWordAlphabetic(inputValue.toLowerCase()));
+    });
+});
+
+function isWordAlphabetic(input) {
+    var currentLetterIndex;
+    for(currentLetterIndex = 0; currentLetterIndex < input.length; currentLetterIndex += 1) {
+        //console.log("Letter: " + input[currentLetterIndex] + " = " + input[currentLetterIndex].charCodeAt());
+        if(currentLetterIndex > 0) {
+            var previousLetter = input[currentLetterIndex - 1];
+            var currentLetter = input[currentLetterIndex];
+            console.log("previousLetter: " + previousLetter);
+            console.log("currentLetter: " + currentLetter);
+            if(previousLetter.charCodeAt() > currentLetter.charCodeAt()) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CodeKatas
+
+1) LettersInAlpahbeticalOrder (https://www.reddit.com/r/dailyprogrammer/comments/3h9pde/20150817_challenge_228_easy_letters_in/)
