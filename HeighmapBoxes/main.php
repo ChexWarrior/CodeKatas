74e4c9204407a00a4526b031030018662e1cd33d
ChexWarrior <aplehm@gmail.com>
2015-10-21 21:27:56 -0400

    Change main to read number or rows and columns from input file

--- HeighmapBoxes/main.php
+++ HeighmapBoxes/main.php
@@ -1,16 +1,31 @@
 <?php
-    //echo "Hello Console!\n";
-    $handle = @fopen("input1.txt","r");
+
+    //1) Determine where squares begin and end
+    //2) Determine if square is within another square
+    //The position of corners is cruical to this...
+
+    $filename = $argv[1];
+    $handle = @fopen($filename,"r");
 
     if($handle) {
-        while(($buffer = fgets($handle, 4096)) !== false) {
-            echo $buffer;
-        }
+        $buffer = fgets($handle, 4096);
+        //read in row and columns lengths 
+        $rowsColumns = explode(" ", trim($buffer));
+        echo "Number of Rows: $rowsColumns[0] \nNumber of Columns: $rowsColumns[1]\n";
+        // while(($buffer =  !== false) {
+        //     echo $buffer;
+        // }
 
-        if(!feof($handle)) {
-            echo "Unexpected Error!\n";
-        }
+        // if(!feof($handle)) {
+        //     echo "Unexpected Error!\n";
+        // }
 
-        fclose($handle);
+        // fclose($handle);
+    } else {
+        echo "$filename could not be opened!\n";
+    }
+
+    function processLine($line) {
+        $newline = $line;
     }
 ?>
