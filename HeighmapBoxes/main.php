f43135ec48bce507a85a2450b4778a42b8236ded
ChexWarrior <aplehm@gmail.com>
2015-10-24 08:48:32 -0400

    Can now store and recongize square sides

--- HeighmapBoxes/main.php
+++ HeighmapBoxes/main.php
@@ -7,6 +7,7 @@
 //Go line by line when we find a square side store this
 //Use number of square lines above us (caclucate) to determine how to draw blank space
 //When we find a matching square side remove it from storage
+//Input file needs newline at end!!
 $filename = $argv[1];
 $handle = fopen($filename,"r");
 //Determines the level of the next square
@@ -18,33 +19,62 @@ if($handle) {
     //read in row and columns lengths 
     $rowsColumns = explode(" ", trim($buffer));
     $numberRows = $rowsColumns[0];
-    $numColumns = $rowsColumns[1];
-    echo "Number of Rows: $numberRows \nNumber of Columns: $numColumns\n";
+    $numberColumns = $rowsColumns[1];
+    echo "Number of Rows: $numberRows \nNumber of Columns: $numberColumns\n";
     //while(($buffer = fgets($handle, 4096)) !== false) {
     for($rowIndex = 0; $rowIndex < $numberRows; $rowIndex += 1) {
         $buffer = fgets($handle, 4096);
-        processLine(str_split($buffer), $currentLevel, $captures, $numColumns, $numberRows);
+        //print_r(str_split($buffer));
+        processLine(str_split($buffer), $currentLevel, $captures, $numberColumns, $rowIndex);
     }
+    echo "\n";
+    print_r($captures);
     fclose($handle);
 } else {
     echo "$filename could not be opened!\n";
 }
 //Read each character in line from left to right:
 //If a + is encountered store this in array 
-function processLine($line, $currentLevel, $captures, $numColumns, $numberRows) {
-    $newLine = $line;
-    foreach($line as $character) {
-        //processCharacter($character, $currentLevel, $captures);
-        echo $character;
+function processLine($line, $currentLevel, &$captures, $numColumns, $currentRow) {
+    $cornerExists = false;
+    $lastSideLength = 0;
+    $container = array();
+    for($colIndex = 0; $colIndex < $numColumns; $colIndex += 1) {
+        if($line[$colIndex] == " ") {
+            //echo fillSpace($currentLevel);
+            echo $line[$colIndex];
+        } else if($line[$colIndex] == "+") {
+            echo $line[$colIndex];
+            $container = determineSquareCapture($captures, $colIndex, $currentRow, $cornerExists, $lastSideLength);
+            $lastSideLength = $container[0];
+            $cornerExists = $container[1];
+        } else if($line[$colIndex] == "-") {
+            echo $line[$colIndex];
+            $lastSideLength += 1;
+        } else {
+            echo $line[$colIndex];
+        }
     }
 }
 
-function processCharacter($character, $currentLevel, $captures) {
-    if($character == " ") {
-        echo fillSpace($currentLevel);
-    } else if($character == "+") {
-
+function determineSquareCapture(&$captures, $currentCol, $currentRow, $cornerExists, $lastSideLength) {
+    // if there is no other + in the array...
+    if($cornerExists) {
+        $cornerExists = false;
+        array_push($captures, array(
+            $lastSideLength,
+            $currentCol,
+            $currentRow
+        ));
+        $lastSideLength = 0;
+    } else {
+        $cornerExists = true;
     }
+
+    return array(
+        $lastSideLength,
+        $cornerExists,
+    );
 }
 
 function fillSpace($currentLevel) {
