6c58a86118fe4c9570444e91d6ac79a30876b87b
Andrew Lehman <aplehm@gmail.com>
2018-06-14 18:18:11 -0400

    Fix error with word length

--- AlphabetCipher/main.py
+++ AlphabetCipher/main.py
@@ -9,24 +9,26 @@ values = (
   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 )
 
-# TODO Handle case when secret_word is longer than message
-while len(secret_word) < len(message):
-  secret_word += secret_word
+secret_word_pad = secret_word
 
-if len(secret_word) > len(message):
-  diff = len(secret_word) - len(message)
-  slice_point = len(secret_word) - diff
+# TODO Handle case when secret_word is longer than message
+while len(secret_word_pad) < len(message):
+  secret_word_pad += secret_word
+
+if len(secret_word_pad) > len(message):
+  diff = len(secret_word_pad) - len(message)
+  slice_point = len(secret_word_pad) - diff
   #print('Slice point is {0}'.format(slice_point))
-  secret_word = secret_word[:slice_point]
+  secret_word_pad = secret_word_pad[:slice_point]
 
 print(message)
-print(secret_word)
+print(secret_word_pad)
 
 i = 0
 for letter in message:
   # print(letter)
-  # print(secret_word[i])
-  col_offset = values.index(secret_word[i])
+  # print(secret_word_pad[i])
+  col_offset = values.index(secret_word_pad[i])
   row_offset = values.index(letter)
   key = col_offset + row_offset
   overflow = key - len(values)
