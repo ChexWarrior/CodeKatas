311324f6f54218e9321a2e3223b3b1bb87cab4af
Chexwarrior <aplehm@gmail.com>
2018-07-06 16:57:59 -0400

    Parse coins correctly

--- ChangeCalculator/main.py
+++ ChangeCalculator/main.py
@@ -1,4 +1,3 @@
-
 file_name = input("Enter file name:\n")
 
 try:
@@ -6,4 +5,15 @@ try:
 except OSError:
   print("File does not exist, exiting...\n")
 
+input_line1 = input_file.readline().split(" ")
+input_line2 = input_file.readline().split(" ")
+
+coins = []
+total = 0
+
+for i in range(1, len(input_line1)):
+  if i == 1: 
+    total = input_line1[i]
+  else:
+    coins.append(input_line1[i])
 
