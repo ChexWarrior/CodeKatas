667005518cf9de27af189dbb7289e2fb55c8c10a
Andrew Lehman <aplehm@gmail.com>
2018-08-16 18:03:53 -0400

    Check for match in word

--- WordHyphenation/main.py
+++ WordHyphenation/main.py
@@ -7,11 +7,25 @@ def scrub_pattern(pattern):
         if char.isalpha():
             scrubbed_pattern += char
 
-    return scrubbed_pattern    
+    return scrubbed_pattern
+
+def is_match(word, pattern, checkStart, checkEnd):
+    if(checkStart):
+        return word.find(pattern) == 0
+    elif(checkEnd):
+        # if the index returned from find + length of scrubbed
+        # pattern is last index
+        return word.find(pattern) + len(pattern) == len(word)
+    else:
+        return pattern in word
 
 def match_patterns(target_word, spaces, patterns):
     for pattern in patterns:
-        print(scrub_pattern(pattern))
+        scrubbed_pattern = scrub_pattern(pattern)
+        checkStart = pattern[0] == '.'
+        checkEnd = pattern[-1] == '.'
+        is_match(target_word, scrubbed_pattern, checkStart, checkEnd)
+
         # check if pattern even matches
 
         # if so then we nee
