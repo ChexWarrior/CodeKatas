f224797db226bc100e6a452ec6c5aae81f86b50f
Andrew Lehman <aplehm@gmail.com>
2018-08-16 17:48:54 -0400

    Create scrub pattern method

--- WordHyphenation/main.py
+++ WordHyphenation/main.py
@@ -1,7 +1,22 @@
 
-def readFile(file):
-    for line in file:
-        print(line)
+# Remove all non alpha characters...
+def scrub_pattern(pattern):
+    scrubbed_pattern = ""
+    
+    for char in pattern:
+        if char.isalpha():
+            scrubbed_pattern += char
+
+    return scrubbed_pattern    
+
+def match_patterns(target_word, spaces, patterns):
+    for pattern in patterns:
+        print(scrub_pattern(pattern))
+        # check if pattern even matches
+
+        # if so then we nee
 
 with open('./patterns.txt') as patterns:
-    readFile(patterns)
+    target_word = input('Enter word to hyphenate...')
+    spaces = len(target_word) - 1
+    match_patterns(target_word, spaces, patterns)
