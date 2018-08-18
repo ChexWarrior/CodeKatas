b0fe46a2cf820f8ff61111e5b18cbb4b2144179b
Andrew Lehman <aplehm@gmail.com>
2018-08-18 16:12:58 -0400

    Check that pattern in word before determining if match is at end

--- WordHyphenation/main.py
+++ WordHyphenation/main.py
@@ -9,26 +9,26 @@ def scrub_pattern(pattern):
 
     return scrubbed_pattern
 
-def is_match(word, pattern, checkStart, checkEnd):
+def check_match(word, pattern, checkStart, checkEnd):
     if(checkStart):
         return word.find(pattern) == 0
     elif(checkEnd):
         # if the index returned from find + length of scrubbed
         # pattern is last index
-        return word.find(pattern) + len(pattern) == len(word)
+        return word.find(pattern) > -1 and word.find(pattern) + len(pattern) == len(word)
     else:
         return pattern in word
 
 def match_patterns(target_word, spaces, patterns):
     for pattern in patterns:
+        pattern = pattern.strip()
         scrubbed_pattern = scrub_pattern(pattern)
         checkStart = pattern[0] == '.'
         checkEnd = pattern[-1] == '.'
-        is_match(target_word, scrubbed_pattern, checkStart, checkEnd)
+        is_match = check_match(target_word, scrubbed_pattern, checkStart, checkEnd)
 
-        # check if pattern even matches
-
-        # if so then we nee
+        if is_match:
+            print("Pattern: %s is a match!" % (pattern))
 
 with open('./patterns.txt') as patterns:
     target_word = input('Enter word to hyphenate...')
